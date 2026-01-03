7ea0ce9f585c3a90af2383c205605d7f4ef975a8
RISHABH KUMAR SINGH <rishabhkumar.singh.23cse@bmu.edu.in>
2026-01-03 15:05:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/contacts", require("./routes/contactRoutes"));
 
 // Start server
-const PORT = 5000;
+// Start server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
